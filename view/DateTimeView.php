71847b9c9edd82ed8554d3b42aacbbde9c2377f5
dl222es <dl222es@student.lnu.se>
2015-09-04 18:28:41 +0200

    added date and time

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -2,11 +2,31 @@
 
 class DateTimeView {
 
-
+        /*
+         * Create a string of the date and time
+         */
 	public function show() {
-
-		$timeString = 'TODO, Write servertime here...';
-
-		return '<p>' . $timeString . '</p>';
+            date_default_timezone_set("Europe/Berlin");
+            $day = date("l");       // Day in the week in letters
+            $number = date("jS");   // The day in number with "st" after
+            $month = date("F");     // Month in letters
+            $year = date("Y");      // Year in letters
+            $time = date("h:i:s");  // The time
+            
+            $hours = intval(date("h"));
+            $minutes = date("i");
+            $seconds = date("s");
+            $midday = date("A");
+            // If it is PM, add 12 hours to get digital time
+            if($midday == "PM")
+            {
+                $hours += 12; 
+            }
+            // add the time to the same variable
+            $time = $hours.":".$minutes.":".$seconds;
+            
+            $timeString = $day.", the ".$number." of ".$month." ".$year.
+                        ", The time is ".$time;
+            return '<p>' . $timeString . '</p>';   
 	}
 }
