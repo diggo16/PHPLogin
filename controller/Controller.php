cbbc4e6588b609918bf4c9c0e96c000e1f11cc56
dl222es <dl222es@student.lnu.se>
2015-09-05 15:21:48 +0200

    fixed error in controller

--- controller/Controller.php
+++ controller/Controller.php
@@ -14,13 +14,34 @@
 class Controller 
 {
     private $loginRules;
+    private $loginView;
+    private $dateTimeView;
+    private $layoutView;
     public function __construct() {
         require_once 'model/LoginRules.php';
+        require_once('view/LoginView.php');
+        require_once('view/DateTimeView.php');
+        require_once('view/LayoutView.php');
+        
         $this->loginRules = new LoginRules();
     }
    public function validateLogin($username, $password)
    {
        $message = $this->loginRules->validateUsername($username);
        return $message;
+   }
+   public function start()
+   {
+        $this->loginView = new LoginView();
+        $this->dateTimeView = new DateTimeView();
+        $this->layoutView = new LayoutView();
+      //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
+        error_reporting(E_ALL);
+        ini_set('display_errors', 'On');
+        
+        $username = $this->loginView->getUsername();
+        //$password = 
+
+        $this->layoutView->render(false, $this->loginView, $this->dateTimeView); 
    }
 }
