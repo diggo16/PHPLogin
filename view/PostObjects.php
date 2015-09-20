772cf0c0062afa017b8214ae08c6151c4f270506
Daniel <dl222es@student.lnu.se>
2015-09-20 14:18:37 +0200

    Added comments

--- view/PostObjects.php
+++ view/PostObjects.php
@@ -1,11 +1,4 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of PostObjects
  *
@@ -13,6 +6,11 @@
  */
 class PostObjects 
 {
+    /**
+     * Check if the POST object is set
+     * @param String $buttonName
+     * @return boolean
+     */
     public function isButtonPushed($buttonName) 
     {
         if(isset($_POST[$buttonName]))
@@ -21,6 +19,11 @@ class PostObjects
         }
         return false;
     }
+    /**
+     * Return the string from the POST object $POSTName
+     * @param String $POSTName
+     * @return String
+     */
     public function getString($POSTName)
     {
         return filter_input(INPUT_POST,$POSTName,FILTER_SANITIZE_STRING);
