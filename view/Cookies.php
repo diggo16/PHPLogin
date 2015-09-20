481163ec0a5277924e4f29be38242737393c6ba0
Daniel <dl222es@student.lnu.se>
2015-09-20 14:01:14 +0200

    Added comments

--- view/Cookies.php
+++ view/Cookies.php
@@ -1,43 +1,48 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Cookies
+ * Class that handles cookies
  *
  * @author Daniel
  */
 class Cookies 
 {
-    /*
+    /**
      * Set the cookie "$cookieName" to the value "$value" 
+     * @param String $cookieName
+     * @param String $value
      */
     public function setCookie($cookieName, $value) 
     {
         $expirationTime = 3600;
         setcookie($cookieName, $value, time() + $expirationTime);
     }
-    /*
+    /**
      * Return the cookie "$cookieName"
+     * @param String $cookieName
+     * @return String $cookieName
      */
     public function getCookie($cookieName) 
     {
         return filter_input(INPUT_COOKIE, $cookieName, FILTER_SANITIZE_STRING);
     }
-    /*
+    /**
      * Clear the cookie "$cookieName"
+     * @param String $cookieName
      */
     public function clearCookie($cookieName)
     {
         setcookie($cookieName,"",time()-1);
     }
+    /**
+     * Generate a secure String 
+     * that becomes a cookie password
+     * @param String $username
+     * @param String $password
+     * @return String cookiePassword
+     */
     public function generateCookiePassword($username, $password)
     {
         $id = $username . "::" . $password;
-        return sha1($id);  
+        return sha1($id);       // sha1 = secure hash algorithm
     }
 }
