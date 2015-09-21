743cdd774d7dd64deaa28d1379844f7f9d553d3b
dl222es <dl222es@student.lnu.se>
2015-09-21 16:20:43 +0200

    Added comment

--- view/LoginView.php
+++ view/LoginView.php
@@ -18,6 +18,9 @@ class LoginView {
         private $post;
         private $cookies;
 
+        /**
+         * Initialize objects
+         */
         public function __construct() 
         {
             require_once 'controller/Controller.php';
