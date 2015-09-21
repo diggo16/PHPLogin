ab19e89eecb45b0c91be0c7ffe83f4d3aab7f461
dl222es <dl222es@student.lnu.se>
2015-09-21 17:12:34 +0200

    Added comments

--- view/LayoutView.php
+++ view/LayoutView.php
@@ -3,36 +3,46 @@
 
 class LayoutView {
   
-  public function render(LoginView $v, DateTimeView $dtv) 
-  {
-      $response = $v->response();
-      $isLoggedIn = $v->isLoggedIn();
-      echo '<!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <title>Login Example</title>
-        </head>
-        <body>
-          <h1>Assignment 2</h1>
-          ' . $this->renderIsLoggedIn($isLoggedIn) . '
-          
-          <div class="container">
-              ' . $response . '
-              
-              ' . $dtv->show() . '
-          </div>
-         </body>
-      </html>
-    ';
-  }
-  
-  private function renderIsLoggedIn($isLoggedIn) {
-    if ($isLoggedIn) {
-      return '<h2>Logged in</h2>';
+    /**
+     * Echo all the views
+     * @param LoginView $v
+     * @param DateTimeView $dtv
+     */
+    public function render(LoginView $v, DateTimeView $dtv) 
+    {
+        $response = $v->response();
+        $isLoggedIn = $v->isLoggedIn();
+        echo '<!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <title>Login Example</title>
+          </head>
+          <body>
+            <h1>Assignment 2</h1>
+            ' . $this->renderIsLoggedIn($isLoggedIn) . '
+
+            <div class="container">
+                ' . $response . '
+
+                ' . $dtv->show() . '
+            </div>
+           </body>
+        </html>
+      ';
     }
-    else {
-      return '<h2>Not logged in</h2>';
+
+    /**
+     * 
+     * @param boolean $isLoggedIn
+     * @return string htmlString
+     */
+    private function renderIsLoggedIn($isLoggedIn) {
+      if ($isLoggedIn) {
+        return '<h2>Logged in</h2>';
+      }
+      else {
+        return '<h2>Not logged in</h2>';
+      }
     }
-  }
 }
