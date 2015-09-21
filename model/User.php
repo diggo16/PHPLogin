1590be68e917e52c07bd492d8ff0a647bcc982be
dl222es <dl222es@student.lnu.se>
2015-09-21 17:35:01 +0200

    Added comment to User

--- model/User.php
+++ model/User.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Description of User
+ * User with information
  *
  * @author Daniel
  */
@@ -13,6 +13,13 @@ class User
     private static $sessionId;
     private static $cookiePassword;
     
+    /**
+     * 
+     * @param string $username
+     * @param string $password
+     * @param boolean $isLoggedIn
+     * @param string $message
+     */
     public function setNewInfo($username, $password, $isLoggedIn, $message) 
     {
         self::$username = $username;
@@ -20,38 +27,73 @@ class User
         self::$isLoggedIn = $isLoggedIn;
         self::$message = $message;
     }
+    /**
+     * Check if the user is logged in
+     * @return boolean
+     */
     public function isLoggedIn() 
     {
         return self::$isLoggedIn;
     }
+    /**
+     * Return the username
+     * @return string $username
+     */
     public function getUsername()
     {
         return self::$username;
     }
+    /**
+     * Return the password
+     * @return string $password
+     */
     public function getPassword()
     {
         return self::$password;
     }
+    /**
+     * Return the message
+     * @return string $message
+     */
     public function getMessage()
     {
         return self::$message;
     }
+    /**
+     * Set the session id
+     * @param string $id
+     */
     public function setSessionId($id)
     {
         self::$sessionId = $id;
     }
+    /**
+     * Return the session id
+     * @return string $sessionId
+     */
     public function getSessionId()
     {
         return self::$sessionId;
     }
+    /**
+     * Set the cookie password
+     * @param string $password
+     */
     public function setCookiePassword($password)
     {
         self::$cookiePassword = $password;
     }
+    /**
+     * Return the cookie password
+     * @return string $cookiePassword
+     */
     public function getCookiePassword()
     {
         return self::$cookiePassword;
     }
+    /**
+     * Log in the user
+     */
     public function login()
     {
         self::$isLoggedIn = true;
