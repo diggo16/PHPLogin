f6a3de870a881f28c8400709b8b237b944aa6745
dl222es <dl222es@student.lnu.se>
2015-09-21 17:19:21 +0200

    Added comments

--- view/Cookies.php
+++ view/Cookies.php
@@ -1,40 +1,45 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Cookies
+ * Class that handles cookies
  *
  * @author Daniel
  */
 class Cookies 
 {
-    /*
+    /**
      * Set the cookie "$cookieName" to the value "$value" 
+     * @param string $cookieName
+     * @param string $value
      */
     public function setCookie($cookieName, $value) 
     {
         $expirationTime = 3600;
         setcookie($cookieName, $value, time() + $expirationTime);
     }
-    /*
+    /**
      * Return the cookie "$cookieName"
+     * @param string $cookieName
+     * @return string $cookieName
      */
     public function getCookie($cookieName) 
     {
         return filter_input(INPUT_COOKIE, $cookieName, FILTER_SANITIZE_STRING);
     }
-    /*
+    /**
      * Clear the cookie "$cookieName"
+     * @param string $cookieName
      */
     public function clearCookie($cookieName)
     {
         setcookie($cookieName,"",time()-1);
     }
+    /**
+     * Generate a secure String 
+     * that becomes a cookie password
+     * @param string $username
+     * @param string $password
+     * @return string cookiePassword
+     */
     public function generateCookiePassword($username, $password)
     {
         $id = $username . "::" . $password;
