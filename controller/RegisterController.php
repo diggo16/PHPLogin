9c3c38472e1e756914e5f93966a2050cebe674a1
Daniel <dl222es@student.lnu.se>
2015-09-25 14:26:22 +0200

    Created RegisterController

---
+++ controller/RegisterController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Description of RegisterController
+ *
+ * @author daniel
+ */
+class RegisterController 
+{
+    public function __construct() 
+    {
+        
+    }
+    public function registerUser($username, $password, $repeatPassword) 
+    {
+        return "false";
+    }
+}
