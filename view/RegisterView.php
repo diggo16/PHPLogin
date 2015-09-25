f5e828c151d25c2717c77ebe1d23d685b796cbe0
Daniel <dl222es@student.lnu.se>
2015-09-25 12:36:03 +0200

    Created RegisterView class

---
+++ view/RegisterView.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Description of RegisterView
+ *
+ * @author daniel
+ */
+class RegisterView 
+{
+    public function __construct() 
+    {
+        
+    }
+    public function generateRegisterForm() 
+    {
+        
+    }
+    public function generateRegisterLink() 
+    {
+        
+    }
+}
