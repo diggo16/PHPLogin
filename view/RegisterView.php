194362320bd4ea0352369a6ad73cb8659203cb2b
Daniel <dl222es@student.lnu.se>
2015-09-29 13:19:48 +0200

    Added comments and TODO to Register

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -18,6 +18,9 @@ class RegisterView
     private static $registration = "RegisterView::Register";    // LoginView::Login
     private static $textLength = 20;
     private static $controller;
+    /**
+     * Initializa objects
+     */
     public function __construct() 
     {
         require_once ('PostObjects.php');
@@ -29,23 +32,37 @@ class RegisterView
         $this->exceptionMsg = new ExceptionMessages();
         $this->feedback = new Feedback();
     }
+    /**
+     * Return a register form if the user don't already have put in valid
+     * registration information
+     * @return string htmlString
+     */
     public function generateRegisterForm() 
     {
         $response = "";
+        // If the registration button is pushed
         if(isset($_POST[self::$registration]))
         {
             $message = $this->checkData();
+            // If there is error in the input values
             if($message != "")
             {
                 $response = $this->getRegisterFormResponse($message, $this->post->getString(self::$username));
             }
         }
+        // Else return an empty registration form 
         else
         {
             $response = $this->getRegisterFormResponse("", "");
         }
         return $response;
     }
+    /**
+     * Return a registration form in html text
+     * @param string $message
+     * @param string $username
+     * @return string htmlString
+     */
     private function getRegisterFormResponse($message, $username)
     {
         return "<h2>Register new user</h2>
@@ -66,68 +83,102 @@ class RegisterView
 					<br/>
 				</fieldset>";
     }
+    /**
+     * Call the controller and return error messages if there was any errors
+     * @return string errorMessages
+     */
     private function checkData()
     {
-        $this->showUsername = true;
+        $this->showUsername = true;     //TODO:unused?
+        // Call the controller to check for errors
         $errors = self::$controller->registerUser($this->post->getStringWithoutFilter(self::$username),
                                                    $this->post->getString(self::$password), 
                                                    $this->post->getString(self::$repeatPassword));
-        return $this->getErrorMessages($errors);
+        return $this->getErrorMessages($errors);    // Translate the error codes to strings
     }
+    /**
+     * Generate a register text link in html
+     * @return string htmlString
+     */
     public function generateRegisterLink() 
     {
-        $response = "<a href='?" . self::$register . "'>Register a new user</a>";
+        $response = "<a href='?" . self::$register . "'>Register a new user</a>";   //TODO refactor with method below?
         return $response;
     }
+    /**
+     * Generate a back to login text link in html
+     * @return string htmlString
+     */
     public function generateBackToLoginLink()
     {
         $response = "<a href='?'>Back to login</a>";
         return $response;
     }
+    /**
+     * Check if the register text link is clicked
+     * @return boolean isRegisterTextClicked
+     */
     public function isRegisterTextClicked()
     {
-        if(isset($_GET[self::$register]))
+        if(isset($_GET[self::$register]))   // TODO change to post->isButtonClicked()
         {
             return true;
         }
         return false;
     }
+    /**
+     * Check if there should be a text link in the response;
+     * @param boolean $isLoggedIn
+     * @return string htmlString
+     */
     public function generateTextLink($isLoggedIn)
     {
         $response = "";
+        // If the user is not logged in
         if(!$isLoggedIn)
         {
             $response = $this->generateRegisterLink();
         }
+        // If the register text is clicked
         if($this->isRegisterTextClicked())
         {
             $response = $this->generateBackToLoginLink();
         }
         return $response;
     }
-    // TODO add messages to feedback
+    /**
+     * Check the array and translate the error codes to strings
+     * @param array $errorArr
+     * @return string errorMessages
+     */
     private function getErrorMessages($errorArr)
     {
+        // TODO refactor?
         $message = "";
+        // If the username is too short
         if(in_array($this->exceptionMsg->getUsernameTooShort(), $errorArr))
         {
             $message .= $this->feedback->getUsernameTooShortMsg();
         }
+        // If the password is too short
         if(in_array($this->exceptionMsg->getPasswordTooShort(), $errorArr))
         {
             $message = $this->ifAddBreak($message);
             $message .= $this->feedback->getPasswordTooShortMsg();
         }
+        // If the username exists
         if(in_array($this->exceptionMsg->getUsernameExists(), $errorArr))    
         {
             $message = $this->ifAddBreak($message);
             $message .= $this->feedback->getUsernameAlreayExists();
         }
+        // If the passwords don't match
         if(in_array($this->exceptionMsg->getPasswordsDontMatch(), $errorArr))
         {
             $message = $this->ifAddBreak($message);
             $message .= $this->feedback->getPasswordsDontMatch();
         }
+        // If the username contains invalid characters
         if(in_array($this->exceptionMsg->getInvalidUsername(), $errorArr))
         {
             $message = $this->ifAddBreak($message);
@@ -135,6 +186,11 @@ class RegisterView
         }
         return $message;
     }
+    /**
+     * Add a break to the string if the messsage isn't empty
+     * @param string $message
+     * @return string $message
+     */
     private function ifAddBreak($message)
     {
         if($message != "")
@@ -143,10 +199,18 @@ class RegisterView
         }
         return $message;
     }
-    public function getSucessfulFeedback()
+    /**
+     * Get the string for a successful registration
+     * @return string successfulRegistration
+     */
+    public function getSucessfulFeedback()  // TODO misspell successful
     {
         return $this->feedback->getSucessfulRegistration();
     }
+    /**
+     * return the newly created user's username
+     * @return string username
+     */
     public function getUsername()
     {
         return $this->post->getString(self::$username);
