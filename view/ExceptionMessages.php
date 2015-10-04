fcb5f31dae1fb3524d75c78a1876121e0e9b50f2
Daniel <dl222es@student.lnu.se>
2015-10-05 01:22:20 +0200

    added comments to ExceptionMessages

--- view/ExceptionMessages.php
+++ view/ExceptionMessages.php
@@ -13,26 +13,50 @@
  */
 class ExceptionMessages 
 {
+    /**
+     * Get username too short string
+     * @return string UsernameShort
+     */
     public function getUsernameTooShort() 
     {
         return "UsernameShort";
     }
+    /**
+     * Get username illegal string
+     * @return string usernameIllegal
+     */
     public function getUsernameIllegal() 
     {
         return "UsernameIllegal";
     }
+    /**
+     * Get username exists string
+     * @return string usernameExists
+     */
     public function getUsernameExists() 
     {
         return "UsernameExists";
     }
+    /**
+     * Get password too short string
+     * @return string passwordTooShort
+     */
     public function getPasswordTooShort()    
     {
         return "PasswordShort";
     }
+    /**
+     * Get passwords dont match string
+     * @return string passwordsDontMatch
+     */
     public function getPasswordsDontMatch()
     {
         return "PasswordsDontMatch";
     }
+    /**
+     * Get invalid username string
+     * @return string invalidUsername
+     */
     public function getInvalidUsername()
     {
         return "UsernameInvalid";
