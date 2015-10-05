7023bd781e08a5d1c0952cb5aea31247bc9539c1
Daniel <dl222es@student.lnu.se>
2015-10-05 02:34:08 +0200

    Refactored getErrorMessages in RegisterView

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -161,36 +161,27 @@ class RegisterView
      */
     private function getErrorMessages($errorArr)
     {
-        // TODO refactor?
         $message = "";
-        // If the username is too short
-        if(in_array($this->exceptionMsg->getUsernameTooShort(), $errorArr))
+        // Array with every exception errors
+        $exceptionArr = array($this->exceptionMsg->getUsernameTooShort(),
+                              $this->exceptionMsg->getPasswordTooShort(),
+                              $this->exceptionMsg->getUsernameExists(),
+                              $this->exceptionMsg->getPasswordsDontMatch(),
+                              $this->exceptionMsg->getInvalidUsername());
+        // Array with every feedback messages
+        $feedbackArr = array($this->feedback->getUsernameTooShortMsg(),
+                             $this->feedback->getPasswordTooShortMsg(),
+                             $this->feedback->getUsernameAlreayExists(),
+                             $this->feedback->getPasswordsDontMatch(),
+                             $this->feedback->getUsernameIsInvalidMsg());
+        // Check if the error is in the errorArray and add a message if there is
+        for($i = 0; $i < count($exceptionArr); $i++)
         {
-            $message .= $this->feedback->getUsernameTooShortMsg();
-        }
-        // If the password is too short
-        if(in_array($this->exceptionMsg->getPasswordTooShort(), $errorArr))
-        {
-            $message = $this->ifAddBreak($message);
-            $message .= $this->feedback->getPasswordTooShortMsg();
-        }
-        // If the username exists
-        if(in_array($this->exceptionMsg->getUsernameExists(), $errorArr))    
-        {
-            $message = $this->ifAddBreak($message);
-            $message .= $this->feedback->getUsernameAlreayExists();
-        }
-        // If the passwords don't match
-        if(in_array($this->exceptionMsg->getPasswordsDontMatch(), $errorArr))
-        {
-            $message = $this->ifAddBreak($message);
-            $message .= $this->feedback->getPasswordsDontMatch();
-        }
-        // If the username contains invalid characters
-        if(in_array($this->exceptionMsg->getInvalidUsername(), $errorArr))
-        {
-            $message = $this->ifAddBreak($message);
-            $message .= $this->feedback->getUsernameIsInvalidMsg();
+            if(in_array($exceptionArr[$i], $errorArr))
+            {
+                $message = $this->ifAddBreak($message);
+                $message .= $feedbackArr[$i];       // add message
+            } 
         }
         return $message;
     }
